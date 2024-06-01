10d3774fd4368f12fd2109af24097552782bf251
Chris DSilva <157317426+ChrisD-7@users.noreply.github.com>
2024-06-01 10:16:54 -0400

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -101,7 +101,7 @@ if option == 'Geospatial Visualization':
 
     # Impact analysis
     scaler = StandardScaler()
-    df['scaled_release_gallons'] = scaler.fit_transform(df[['max_ptl_release_gallons']]) * 100000
+    df['scaled_release_gallons'] = scaler.fit_transform(df[['max_ptl_release_gallons']]) * 1000
     impact_layer = pdk.Layer(
         'ScatterplotLayer',
         data=df,
